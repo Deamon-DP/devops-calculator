a153f1f12d118a880ead92b6e7461e6e7d069961
Deamon_DP <durgaprasadr17@gmail.com>
2023-03-12 03:44:09 +0530

    basic calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,167 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState("0");
+  const [expression, setExpression] = useState('');
+
+
+
+  function handleClick(event) {
+    const value = event.target.value;
+    if (value === '=') {
+      calculateResult();
+    }
+    else if (value === 'C') {
+      clearScreen();
+    }
+    else if (value === 'DEL') {
+      deleteLastCharacter();
+    }
+
+    else {
+      if (input !== 'Error' && input !== '0') {
+        setExpression(prevExpression => prevExpression + value);
+
+        setInput(prevInput => prevInput + value);
+      }
+      else {
+        setInput(value);
+        setExpression(value);
+      }
+    }
+  }
+
+
+
+  function calculateResult() {
+    try {
+
+      if (input.slice(-1) === "!") {
+
+        evaluateFactorial();
+        return
+      }
+      else {
+        const resultFn = new Function('return ' + expression);
+        const result = resultFn();
+        if (result === Infinity || result === -Infinity) {
+          throw new Error('Cannot divide by zero');
+        }
+        setInput(result);
+      }
+    } catch (error) {
+      setInput('Error');
+    }
+  }
+
+
+
+
+  function factorial(n) { if (n === 0 || n === 1) { return 1; } else { return n * factorial(n - 1); } }
+
+
+
+  function evaluateFactorial() {
+    
+
+    const resultFn = new Function('return ' + expression.slice(0,-1));
+    const n = resultFn();
+    if (n >= 0) {
+      //   let factorialString = "function factorial(n) { if (n === 0 || n === 1) { return 1; } else { return n * factorial(n-1); } }";
+
+      // // Use the Function() constructor to create a new function from the string
+      // let factorial = new Function(factorialString);
+
+
+      let result = factorial(n);
+      console.log(result);
+      setExpression(result);
+      setInput(result);
+
+    }
+    else {
+      setInput('Error');
+    }
+
+  }
+
+  function clearScreen() {
+    setExpression('');
+    setInput("0");
+  }
+  function deleteLastCharacter() {
+    setExpression(prevExpression => prevExpression.slice(0, -1));
+    setInput(prevInput => prevInput.slice(0, -1));
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div data-testid="calculator" className="calculator">
+      <div className="input">{input}</div>
+      <div className="row">
+        <button className="button" value="(" onClick={handleClick}>(</button>
+        <button className="button" value=")" onClick={handleClick}>)</button>
+        <button className="button" value="Math.PI" onClick={handleClick}>π</button>
+        <button className="button" value="Math.E" onClick={handleClick}>e</button>
+      </div>
+      <div className="row">
+        <button className="button" value="Math.sqrt(" onClick={handleClick}>√</button>
+        <button className="button" value="Math.sin(" onClick={handleClick}>sin</button>
+        <button className="button" value="Math.cos(" onClick={handleClick}>cos</button>
+        <button className="button" value="Math.tan(" onClick={handleClick}>tan</button>
+      </div>
+      <div className="row">
+        <button className="button" value="!" onClick={handleClick}>!</button>
+        <button className="button" value="Math.log10(" onClick={handleClick}>log</button>
+      </div>
+      <div className="row">
+        <button className="button" value="Math.exp(" onClick={handleClick}>exp</button>
+        <button className="button" value="Math.abs(" onClick={handleClick}>|x|</button>
+        <button className="button" value="Math.floor(" onClick={handleClick}>⌊x⌋</button>
+        <button className="button" value="Math.ceil(" onClick={handleClick}>⌈x⌉</button>
+      </div>
+      <div className="row">
+        <button className="button" value="Math.round(" onClick={handleClick}>round</button>
+        <button className="button" value="**" onClick={handleClick}>^</button>
+      </div>
+      <div className="row">
+        <button className="button" value="7" onClick={handleClick}>7</button>
+        <button className="button" value="8" onClick={handleClick}>8</button>
+        <button className="button" value="9" onClick={handleClick}>9</button>
+        <button className="button" id="clear" value="C" onClick={handleClick}>AC</button>
+        <button className="button" id="delete" value="DEL" onClick={handleClick}>DEL</button>
+
+      </div>
+      <div className="row">
+        <button className="button" value="4" onClick={handleClick}>4</button>
+        <button className="button" value="5" onClick={handleClick}>5</button>
+        <button className="button" value="6" onClick={handleClick}>6</button>
+        <button className="button" id="multiply" value="*" onClick={handleClick}>*</button>
+        <button className="button" id="divide" value="/" onClick={handleClick}>/</button>
+
+      </div>
+      <div className="row">
+        <button className="button" value="1" onClick={handleClick}>1</button>
+        <button className="button" value="2" onClick={handleClick}>2</button>
+        <button className="button" value="3" onClick={handleClick}>3</button>
+        <button className="button" id="add" value="+" onClick={handleClick}>+</button>
+        <button className="button" id="minus" value="-" onClick={handleClick}>-</button>
+      </div>
+      <div className="row">
+        <button className="button" value="0" onClick={handleClick}>0</button>
+        <button className="button" value="." onClick={handleClick}>.</button>
+        <button className="button" id="mod" value="%" onClick={handleClick}>%</button>
+
+        <button className="equals-button" id="equals" value="=" onClick={handleClick}>=</button>
+
+      </div>
+
     </div>
   );
 }
 
 export default App;
+
+
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,131 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Calculator App', () => {
+  test('renders calculator component', () => {
+     const { getByTestId } = render(<App />);
+     const calculator = getByTestId("calculator");
+     expect(calculator).toBeInTheDocument();
+  });
+
+  test('displays input on screen when button is clicked', () => {
+    const { getByTestId } = render(<App />);
+    const calculator = getByTestId('calculator');
+    const screen = calculator.querySelector('.input');
+    const button = calculator.querySelector('.button[value="1"]');
+    fireEvent.click(button);
+    expect(screen).toHaveTextContent('1');
+  });
+
+  test('evaluates input and displays result on screen when "=" button is clicked', () => {
+    const { getByTestId } = render(<App />);
+    const calculator = getByTestId('calculator');
+    const screen = calculator.querySelector('.input');
+    const button1 = calculator.querySelector('.button[value="1"]');
+    const button2 = calculator.querySelector('.button[value="+"]');
+    const button3 = calculator.querySelector('.button[value="2"]');
+    const equalsButton = calculator.querySelector('.equals-button[value="="]');
+    fireEvent.click(button1);
+    fireEvent.click(button2);
+    fireEvent.click(button3);
+    fireEvent.click(equalsButton);
+    expect(calculator).toHaveTextContent('3');
+  });
+  test('displays error message when dividing by zero', () => {
+    const { getByTestId } = render(<App />);
+    const calculator = getByTestId('calculator');
+    const screen = calculator.querySelector('.input');
+    const button1 = calculator.querySelector('.button[value="1"]');
+    const button2 = calculator.querySelector('.button[value="/"]');
+    const button3 = calculator.querySelector('.button[value="0"]');
+    const equalsButton = calculator.querySelector('.equals-button[value="="]');
+    fireEvent.click(button1);
+    fireEvent.click(button2);
+    fireEvent.click(button3);
+    fireEvent.click(equalsButton);
+    //expect(screen).toHaveClass('Error');
+    expect(calculator).toHaveTextContent('Error');
+  });
+
+
+
+  test('checks delete button', () => {
+    const { getByTestId } = render(<App />);
+    const calculator = getByTestId('calculator');
+    const screen = calculator.querySelector('.input');
+    const button1 = calculator.querySelector('.button[value="1"]');
+    const button2 = calculator.querySelector('.button[value="2"]');
+
+    const button3 = calculator.querySelector('.button[value="DEL"]');
+    fireEvent.click(button1);
+    fireEvent.click(button2);
+    fireEvent.click(button3);
+
+    expect(screen).toHaveTextContent('1');
+  });
+
+
+  test('evaluates substraction operation', () => {
+    const { getByTestId } = render(<App />);
+    const calculator = getByTestId('calculator');
+    const screen = calculator.querySelector('.input');
+    const button1 = calculator.querySelector('.button[value="1"]');
+    const button2 = calculator.querySelector('.button[value="-"]');
+    const button3 = calculator.querySelector('.button[value="2"]');
+    const equalsButton = calculator.querySelector('.equals-button[value="="]');
+    fireEvent.click(button1);
+    fireEvent.click(button2);
+    fireEvent.click(button3);
+    fireEvent.click(equalsButton);
+    expect(calculator).toHaveTextContent('-1');
+  });
+
+  test('evaluates multiplication operation', () => {
+    const { getByTestId } = render(<App />);
+    const calculator = getByTestId('calculator');
+    const screen = calculator.querySelector('.input');
+    const button1 = calculator.querySelector('.button[value="8"]');
+    const button2 = calculator.querySelector('.button[value="*"]');
+    const button3 = calculator.querySelector('.button[value="6"]');
+    const equalsButton = calculator.querySelector('.equals-button[value="="]');
+    fireEvent.click(button1);
+    fireEvent.click(button2);
+    fireEvent.click(button3);
+    fireEvent.click(equalsButton);
+    expect(calculator).toHaveTextContent('48');
+  });
+
+  test('evaluates division operation', () => {
+    const { getByTestId } = render(<App />);
+    const calculator = getByTestId('calculator');
+    const screen = calculator.querySelector('.input');
+    const button1 = calculator.querySelector('.button[value="8"]');
+    const button2 = calculator.querySelector('.button[value="/"]');
+    const button3 = calculator.querySelector('.button[value="2"]');
+    const equalsButton = calculator.querySelector('.equals-button[value="="]');
+    fireEvent.click(button1);
+    fireEvent.click(button2);
+    fireEvent.click(button3);
+    fireEvent.click(equalsButton);
+    expect(calculator).toHaveTextContent('4');
+  });
+
+  test('evaluates square-root operation', () => {
+    const { getByTestId } = render(<App />);
+    const calculator = getByTestId('calculator');
+    const screen = calculator.querySelector('.input');
+    const button1 = calculator.querySelector('.button[value="Math.sqrt("]');
+    const button2 = calculator.querySelector('.button[value="9"]');
+    const button3 = calculator.querySelector('.button[value=")"]');
+
+    const equalsButton = calculator.querySelector('.equals-button[value="="]');
+    fireEvent.click(button1);
+    fireEvent.click(button2);
+    fireEvent.click(button3);
+    fireEvent.click(equalsButton);
+    expect(calculator).toHaveTextContent('3');
+  });
+
+
 });
